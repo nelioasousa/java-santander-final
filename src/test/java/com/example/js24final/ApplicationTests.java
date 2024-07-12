a8390b57806ee91b684de8f70dce6747cf6e81e5
nelioasousa <corneliosousaphb@gmail.com>
2024-07-12 12:23:18 -0300

    Spring Initializr + Minor additions & modifications

---
+++ src/test/java/com/example/js24final/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.js24final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
